06070e79c061954621ac25eb196e75a68d04f634
SoloChiou <chingshuen5@gmail.com>
2024-09-09 16:40:47 +0800

    dev prod setting

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+# flask app.py
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # if dockerfile set ENV PORT, get it.
+    port = int(os.environ.get('PORT', 5000))
+    # dev debug -> True, prod debug --> False
+    debug = os.environ.get('FLASK_DEBUG', 'False') == 'True'
+    app.run(debug=debug, host='0.0.0.0', port=port)
